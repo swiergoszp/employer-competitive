bb9282bfd6a760cfb39c5e0b307c3957fa6e855b
Paul <swiergoszp@gmail.com>
2018-07-20 01:03:38 -0600

    overhaul

---
+++ assets/javascript/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var swiper = new Swiper('.swiper-container', {
+        effect: 'cube',
+        grabCursor: true,
+        cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+        },
+            pagination: {
+            el: '.swiper-pagination',
+            },
+    });
+});
